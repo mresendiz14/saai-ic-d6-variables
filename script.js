cf10de5221a9c19b425e124c8b64ac157ee1e83e
mresendiz14 <107743452+mresendiz14@users.noreply.github.com>
2024-11-08 04:11:25 +0000

    added script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let animal = "bear"
+
+console.log("Awesome I'm now writing JavaScript")
+console.log("This is the 2nd line")
+console.log(animal)
+console.log("I'm still here...")
+
+//String
+let firstName = "Maribel"
+console.log(firstName)
+
+let currentAge = 27
+console.log("initial age", currentAge)
+
+currentAge = 28
+console.log("updated age", currentAge)
+
+let isAlive = true
+console.log(true)
